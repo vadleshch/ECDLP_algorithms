5f439ff7bb8f3b11467cd748d4ec97948d3dc639
user <user@DESKTOP-TPJM3KM>
2026-08-27 15:55:23 +0300

    benchmark Lambda and BSGS

--- ECDLP_algorithms/Program.cs
+++ ECDLP_algorithms/Program.cs
@@ -1,1 +1,189 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ECDLP_algorithms;
+using ECDLP_algorithms.Algorithms;
+using Org.BouncyCastle.Security;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
+using Int = Org.BouncyCastle.Math.BigInteger;
+
+//TestBSGS();
+TestLambda();
+
+static void TestBSGS()
+{
+    int[] lengths = { 16, 24, 32, 40, 48 };
+    int tests = 20;
+
+    Console.WriteLine("BSGS");
+
+    BSGS.Solve(secp256k1Group.G, secp256k1Group.G.Multiply(Int.ValueOf(123)), Int.ValueOf(256));
+
+    foreach (int length in lengths)
+    {
+        double time = 0;
+        double memory = 0;
+        Int r = Int.One.ShiftLeft(length);
+
+        for (int i = 0; i < tests; i++)
+        {
+            Int k = new Int(length, new Org.BouncyCastle.Security.SecureRandom());
+            var Q = secp256k1Group.G.Multiply(k);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            long startMemory = GC.GetTotalMemory(true);
+            long maxMemory = startMemory;
+
+            ManualResetEventSlim finished = new ManualResetEventSlim(false);
+
+            Thread monitor = new Thread(() =>
+            {
+                while (!finished.IsSet)
+                {
+                    long currentMemory = GC.GetTotalMemory(false);
+
+                    if (currentMemory > maxMemory)
+                    {
+                        maxMemory = currentMemory;
+                    }
+
+                    Thread.Sleep(1);
+                }
+            });
+
+            monitor.IsBackground = true;
+            monitor.Start();
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Int result = BSGS.Solve(secp256k1Group.G, Q, r);
+            stopwatch.Stop();
+
+            long endMemory = GC.GetTotalMemory(false);
+
+            if (endMemory > maxMemory)
+            {
+                maxMemory = endMemory;
+            }
+
+            finished.Set();
+            monitor.Join();
+
+            if (!result.Equals(k))
+            {
+                throw new Exception("Wrong result");
+            }
+
+            time += stopwatch.Elapsed.TotalSeconds;
+            memory += (maxMemory - startMemory) / 1024.0 / 1024.0;
+        }
+
+        Console.WriteLine($"Key length: {length} bit");
+        Console.WriteLine($"Average time: {time / tests:F3} s");
+        Console.WriteLine($"Average memory: {memory / tests:F3} MB\n");
+    }
+}
+
+static void TestLambda()
+{
+    int[] lengths = { 16, 24, 32, 40, 48 };
+    int tests = 1;
+    int threads = 5;
+
+    Console.WriteLine("Lambda Pollard");
+
+    foreach (int length in lengths)
+    {
+        double time = 0;
+        double memory = 0;
+
+        Int a = Int.Zero;
+        Int b = Int.One.ShiftLeft(length).Subtract(Int.One);
+
+        for (int i = 0; i < tests; i++)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            long startMemory = GC.GetTotalMemory(true);
+            long maxMemory = startMemory;
+            bool running = true;
+
+            Thread monitor = new Thread(() =>
+            {
+                while (running)
+                {
+                    long currentMemory = GC.GetTotalMemory(false);
+
+                    if (currentMemory > maxMemory)
+                    {
+                        maxMemory = currentMemory;
+                    }
+
+                    Thread.Sleep(1);
+                }
+            });
+
+            monitor.IsBackground = true;
+            monitor.Start();
+
+            Task[] tasks = new Task[threads];
+            double[] times = new double[threads];
+            Barrier barrier = new Barrier(threads);
+
+            for (int j = 0; j < threads; j++)
+            {
+                int index = j;
+
+                tasks[j] = Task.Run(() =>
+                {
+                    Int k = new Int(length, new SecureRandom());
+                    var Q = secp256k1Group.G.Multiply(k);
+
+                    barrier.SignalAndWait();
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    Int result = LambdaPollard.Solve(secp256k1Group.G, Q, a, b, CancellationToken.None);
+                    stopwatch.Stop();
+
+                    if (!result.Equals(k))
+                    {
+                        throw new Exception("Wrong result");
+                    }
+
+                    times[index] = stopwatch.Elapsed.TotalSeconds;
+                });
+            }
+
+            Task.WaitAll(tasks);
+
+            running = false;
+            monitor.Join();
+
+            long endMemory = GC.GetTotalMemory(false);
+
+            if (endMemory > maxMemory)
+            {
+                maxMemory = endMemory;
+            }
+
+            double testTime = 0;
+
+            for (int j = 0; j < threads; j++)
+            {
+                testTime += times[j];
+            }
+
+            testTime /= threads;
+
+            time += testTime;
+            memory += (maxMemory - startMemory) / 1024.0 / 1024.0;
+        }
+        Console.WriteLine($"\nKey length: {length} bit");
+        Console.WriteLine($"Average time: {time / tests:F3} s");
+        Console.WriteLine($"Average memory: {memory / tests:F3} MB\n");
+    }
+}
